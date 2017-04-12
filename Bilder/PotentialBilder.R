ee642ed7ecc9e2723dee4cf8e43aeed29643e127
Lukas Graf <lukas.matthias.graf@student.uni-augsburg.de>
2017-04-12 16:52:04 +0200

    Beweise der Potentialcharakterisierungssätze besser strukturiert und vereinheitlicht

--- Bilder/PotentialBilder.R
+++ Bilder/PotentialBilder.R
@@ -1,5 +1,9 @@
 library(plot3D)
 
+drawRaster <- function(xdims) {
+  persp3D(1:xdims[1], 1:xdims[2], 0, border="black", facets = FALSE, add=TRUE, colkey = FALSE)
+}
+
 drawTopografie <- function(costs, xdims, tileColor, zdim=max(costs), newPersp=TRUE) {
   if(newPersp) 
     persp3D(1:xdims[1], 1:xdims[2], costs, border="black", col=tileColor, facets = TRUE, add=FALSE, xlim=c(1,xdims[1]), ylim=c(1,xdims[2]), zlim=c(0,zdim), d=100000, colkey = FALSE)
@@ -44,5 +48,6 @@ c2<-matrix(c(4,4,0,7,1,4,7,4,4), nrow=3)
 P<-matrix(c(3,5,1,6,2,5,6,5,5), nrow=3)
 drawGame(c1,c2,c(3,3), P)
 
+drawRaster(c(3,3))
 drawTopografie(c1,c(3,3),tileColor=alpha.col(col=rgb(0.6,0.1,0.2), alpha=0.7), zdim=15)
 drawTopografie(c2,c(3,3),tileColor=alpha.col(col=rgb(0.1,0.1,0.8), alpha=0.7), zdim=15)
